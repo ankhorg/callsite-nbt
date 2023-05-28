0103721e98ed18c6262b0d25df5fa83a1ea92872
InkerBot <im@inker.bot>
2023-05-29 07:57:49 +0800

    add 1.12.2 support

---
+++ nms/build.gradle.kts
@@ -0,0 +1,16 @@
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://r.irepo.space/maven")
+}
+
+dependencies {
+    compileOnly("bot.inker.ulmc.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
+    compileOnly("bot.inker.ulmc.paper:paper:1.12.2-R0.1-SNAPSHOT")
+
+    // compileOnly("bot.inker.ulmc.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    // compileOnly("bot.inker.ulmc.paper:paper:1.16.5-R0.1-SNAPSHOT")
+
+    // compileOnly("bot.inker.ulmc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    // compileOnly("bot.inker.ulmc.paper:paper-server:1.19.4-R0.1-SNAPSHOT:mojang-mapped")
+}
