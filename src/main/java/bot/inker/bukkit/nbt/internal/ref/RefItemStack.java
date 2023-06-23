6b2462f03084fe6e1d3fe3bbc7bb3276ef3adf2b
Neige <72865616+Neige7@users.noreply.github.com>
2023-06-23 10:22:21 +0800

    add RefCraftItemStack

---
+++ src/main/java/bot/inker/bukkit/nbt/internal/ref/RefItemStack.java
@@ -0,0 +1,9 @@
+package bot.inker.bukkit.nbt.internal.ref;
+
+import bot.inker.bukkit.nbt.internal.annotation.CbVersion;
+import bot.inker.bukkit.nbt.internal.annotation.HandleBy;
+import org.bukkit.inventory.ItemStack;
+
+@HandleBy(version = CbVersion.v1_12_R1, reference = "net/minecraft/server/v1_12_R1/ItemStack")
+public class RefItemStack {
+}
